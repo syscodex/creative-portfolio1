bb84658d3b91d98e2a0c1af3cab0c9baaaa0b009
Vincent <77495110+syscodex@users.noreply.github.com>
2024-01-23 20:42:50 +0800

    refactor: Update image handling to use object-cover
    Replace the deprecated layout="fill" prop with the object-cover class to ensure proper handling of images within their containers. This change maintains the image aspect ratio while covering the entire container. Also, removed explicit dimensions for better flexibility.

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -59,17 +59,22 @@ export default function Avatar ({
     }, component);
   },[]);
 
-    return (
+  return (
     <div ref={component} className={clsx("relative h-full w-full", className)}>
-        <div className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0f">
-        <PrismicNextImage 
-        field={image} 
-        className="avatar-image h-full w-full object-fill"
-        imgixParams={{q: 90}}
+      <div
+        className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0"
+        style={{ perspective: "500px", perspectiveOrigin: "150% 150%", position: "relative" }}
+      >
+        <PrismicNextImage
+          field={image}
+          className="avatar-image h-full w-full object-cover"
+          imgixParams={{ q: 90 }}
         />
-
-        <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via white to-transparent opacity-0 md:block"></div>
+        <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
       </div>
     </div>
-    );
+  )
 }
+
+  
+  
